99fbb6e0cefe3708faec529cd702207aa497b683
Asmit Chhotaray <asmitchhotaray@gmail.com>
2026-02-21 10:53:44 +0530

    Create the first section

---
+++ src/components/Page1.jsx
@@ -0,0 +1,16 @@
+export function Page1() {
+  return (
+   <section id="hero" className="w-full h-screen flex flex-col items-center justify-center gap-[1em] relative z-10">
+        <h1 className="text-9xl text-center">
+          digital <br /> Evolution
+        </h1>
+
+        <p className="text-3xl tracking-tight">transform your brand identity</p>
+        <p className="w-[50%] text-[13px] text-center font-bolc">
+          experience the next generation of digital products with emmersive
+          experience that blends innovation with functionality,helping brand
+          standout in the digital landscape
+        </p>
+   </section>
+  );
+}
