27c81ccc71457b363f518b22a460e5f4a0e0cc91
Asmit Chhotaray <asmitchhotaray@gmail.com>
2026-02-21 11:27:49 +0530

    create the page4 or outro page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Page1 } from './components/Page1'
 import { Page2 } from './components/Page2'
 import { Page3 } from './components/Page3'
+import { Page4 } from './components/Page4'
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Page1/>
       <Page2/>
       <Page3/>
+      <Page4/>
     </>
   )
 }
